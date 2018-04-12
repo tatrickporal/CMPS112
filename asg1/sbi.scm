be5309470adf3d67c739f0dfcd05232b07fb48dc
pt <ptoral32@gmail.com>
2018-04-12 16:07:16 -0700

    Sbi changes

--- asg1/sbi.scm
+++ asg1/sbi.scm
@@ -41,10 +41,7 @@
 )
 
 (define (iterate_through_program program)
-	(if (not(null? program)
-		(map (lambda (line) (printf "~s~n" line)) (iterate_through_program(car program)))
-		)
-	)
+	(map (lambda (line) (printf "~s~n" line)) (iterate_through_program(car program)))	
 )
 
 (define (write-program-by-line filename program)
