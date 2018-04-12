92b906dbafe42426c0a43aadf937bec372a3d701
pt <ptoral32@gmail.com>
2018-04-11 22:17:14 -0700

    Changes

--- asg1/sbi.scm
+++ asg1/sbi.scm
@@ -31,42 +31,13 @@
     (die `("Usage: " ,*run-file* " filename"))
 )
 
-(define *symbol-table* (make-hash))
-(define (symbol-get key)
-        (hash-ref *symbol-table* key))
-(define (symbol-put! key value)
-        (hash-set! *symbol-table* key value))
-
-(for-each
-    (lambda (pair)
-            (symbol-put! (car pair) (cadr pair)))
-    `(
-
-        (log10_2 0.301029995663981195213738894724493026768189881)
-        (sqrt_2  1.414213562373095048801688724209698078569671875)
-        (e       2.718281828459045235360287471352662497757247093)
-        (pi      3.141592653589793238462643383279502884197169399)
-        (div     ,(lambda (x y) (floor (/ x y))))
-        (log10   ,(lambda (x) (/ (log x) (log 10.0))))
-        (mod     ,(lambda (x y) (- x (* (div x y) y))))
-        (quot    ,(lambda (x y) (truncate (/ x y))))
-        (rem     ,(lambda (x y) (- x (* (quot x y) y))))
-        (+       ,+)
-        (^       ,expt)
-        (ceil    ,ceiling)
-        (exp     ,exp)
-        (floor   ,floor)
-        (log     ,log)
-        (sqrt    ,sqrt)
-     ))
-
 (define (readlist-from-inputfile filename)
     (let ((inputfile (open-input-file filename)))
          (if (not (input-port? inputfile))
              (die `(,*run-file* ": " ,filename ": open failed"))
              (let ((program (read inputfile)))
                   (close-input-port inputfile)
-                         program))))
+                  program))))
 
 (define (write-program-by-line filename program)
     (printf "==================================================~n")
@@ -87,22 +58,6 @@
         (usage-exit)
         (let* ((sbprogfile (car arglist))
              	(program (readlist-from-inputfile sbprogfile)))
-            	(write-program-by-line sbprogfile program)))
-				(symbol-put! 'n (expt 2.0 32.0))
-				(symbol-put! 'a (make-vector 10 0.0))
-				(vector-set! (symbol-get 'a) 3 (symbol-get 'pi))
-				(printf "2 ^ 16 = ~s~n" ((symbol-get '^) 2.0 16.0))
-				(printf "log 2 = ~s~n" ((symbol-get 'log) 2.0))
-				(printf "log10 2 = ~s~n" ((symbol-get 'log10) 2.0))
-				(printf "THIS IS PI = ~s~n" ((symbol-get 'log10) 2.0))
-				
-				(newline)
-				(printf "*symbol-table*:~n")
-				(hash-for-each *symbol-table*
-					(lambda (key value)
-						(printf "~s : ~s = ~s~n" key (what-kind value) value))
-				    ))
-
+            	(write-program-by-line sbprogfile program))))
 		
-
 (main (vector->list (current-command-line-arguments)))
