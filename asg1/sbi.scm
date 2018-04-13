9d88711242ce62c7e1835c234d464a820faaa948
pt <ptoral32@gmail.com>
2018-04-13 02:11:19 -0700

    Sbi

--- asg1/sbi.scm
+++ asg1/sbi.scm
@@ -61,7 +61,7 @@
 
 (define (iterate_through_line line)
 	(when (not (null? list))
-		( (lambda (symbol) (printf "~s~n okayy" symbol))  )
+		( map (lambda (symbol) (printf "~s~n okayy" symbol))  line)
 	(iterate_through_line (cdr line)))  
 	
 )
