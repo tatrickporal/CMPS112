dc69abbb2412f8deb6693b15cb4ec1030f0debbf
pt <ptoral32@gmail.com>
2018-04-12 20:58:26 -0700

    sbi

--- asg1/sbi.scm
+++ asg1/sbi.scm
@@ -49,7 +49,7 @@
     (printf "==================================================~n")
     (printf "~a: ~s~n" *run-file* filename)
     (printf "==================================================~n")
-    (map (lambda (line) (printf "~s~n" line))  (cadr program))
+    (map (lambda (line) (printf "~s~n" line))  (cadddr program))
 )
     
 (define (what-kind value)
