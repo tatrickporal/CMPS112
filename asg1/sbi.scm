e81d38fa0d889822f97fb32ba95ae04a431a2ece
pt <ptoral32@gmail.com>
2018-04-13 13:00:03 -0700

    Sbi

--- asg1/sbi.scm
+++ asg1/sbi.scm
@@ -61,7 +61,7 @@
 
 (define (iterate_through_line line)
 	(when (not (null? line))
-		  (printf "~s~n okayy" (cdr line))
+		  (printf "~s~n okayy" (car line))
 	(iterate_through_line (cdr line)))  
 	
 )
