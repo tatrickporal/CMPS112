d5f6680b7ee0f3df0ddb3844691eb01ee0f38651
pt <ptoral32@gmail.com>
2018-04-14 12:34:09 -0700

    Sbi

--- asg1/sbi.scm
+++ asg1/sbi.scm
@@ -77,7 +77,7 @@
     (printf "==================================================~n")
     (map 
     	(lambda (line) 
-    		(parse_line (cdr line) )  
+    		(parse_line (cadr line) )  
     	)  
     program)
 )
