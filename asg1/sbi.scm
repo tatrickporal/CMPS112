2e6b96bc9a0b78a4ae6401e5b7a6e78bfbf5906b
pt <ptoral32@gmail.com>
2018-04-14 13:05:21 -0700

    Sbi

--- asg1/sbi.scm
+++ asg1/sbi.scm
@@ -67,7 +67,7 @@
 
 (define (parse_line line)
 	(when (not (null? line))
-		  (printf "~s~n"  (cadr line))
+		  (printf "~s~n"  (cdr line))
 	)
 )
 
@@ -77,7 +77,7 @@
     (printf "==================================================~n")
     (map 
     	(lambda (line) 
-    		(parse_line  line )  
+    		(parse_line  (car line )  
     	)  
     program)
 )
