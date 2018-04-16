b94957f50f84511c15574a68cf148a980e503301
pt <ptoral32@gmail.com>
2018-04-16 02:36:30 -0700

    Sbi

--- asg1/sbi.scm
+++ asg1/sbi.scm
@@ -86,9 +86,9 @@
 (define (decifer line)
 	(when (not(null? line))
 		(cond 
-			((pair? line) (printf "~s~n" line)
+			((pair? line) (printf "~s~n" line))
 			(else (printf "this please"))
-			)
+			
 
 		)
 		
