e66ab659565b60a85ec725cbf5ccbeba6c6e62fc
pt <ptoral32@gmail.com>
2018-04-16 10:22:16 -0700

    Sbi

--- asg1/sbi.scm
+++ asg1/sbi.scm
@@ -86,7 +86,7 @@
 (define (decifer line)
 	(when (not(null? line))
 		(cond 
-			((pair? line) (printf "~s~n" line))
+			((pair? line) (printf " has been changed~s~n" line))
 			(else (printf "this please"))
 			
 
