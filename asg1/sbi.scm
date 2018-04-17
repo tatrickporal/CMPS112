816c7af453168b4ff5bfb0cb48099885ef6d020c
pt <ptoral32@gmail.com>
2018-04-17 03:00:38 -0700

    sbi

--- asg1/sbi.scm
+++ asg1/sbi.scm
@@ -44,7 +44,7 @@
 (define (write-program-by-line filename program)
 
     (map (lambda (line) 
-    		(decifer  line)
+    		(decifer  (cdr line))
     	) program)
     ;;(decode program)
 )
@@ -106,7 +106,7 @@
 	
  ;;(let (())
 	(when(not(null? line))
-		(printf "~s~n" ( cdar line))
+		(printf "~s~n" ( cdr line))
 		(cond ((symbol? (car line)) (hash-set! (cadr line) line))
 			(printf "~s~n" line)
 		)
