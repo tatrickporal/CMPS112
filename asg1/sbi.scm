439e99f5ccfcdddd9c3bc0990547c17919cb0afe
pt <ptoral32@gmail.com>
2018-04-17 10:46:08 -0700

    sbi

--- asg1/sbi.scm
+++ asg1/sbi.scm
@@ -109,7 +109,7 @@
  		((eqv? command 'print)
  			(cond ((= 1 state_len) (printf "~n")) 
  				((= 2 state_len) 
- 					(display    (car statement))
+ 					(display    (cadr statement))
  					(newline)
  				)
  			)
