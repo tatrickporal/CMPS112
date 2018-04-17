6395c77ceea6f2b6869d3d83f2cf12997be65d6a
pt <ptoral32@gmail.com>
2018-04-17 03:13:39 -0700

    sbi

--- asg1/sbi.scm
+++ asg1/sbi.scm
@@ -110,10 +110,10 @@
 		
 		(cond ((symbol? (car line)) (hash-set! (cadr line) line)) ;; if label put into label-table
 			((= 3 (length statement))
-				(printf "~s~n"  (caddr statement))
+				(printf "a threesome ~s~n"  (caddr statement))
 			)
 			((pair? statement)
-				(printf "~s~n"  (cdr statement)) 
+				(printf "a pair ~s~n"  (cdr statement)) 
 			)
 
 		)
