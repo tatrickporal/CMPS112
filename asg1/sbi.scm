e2c6c62d89b004e42c0901928cb0ae1748432572
pt <ptoral32@gmail.com>
2018-04-17 22:52:12 -0700

    sbi

--- asg1/sbi.scm
+++ asg1/sbi.scm
@@ -113,7 +113,7 @@
 			((pair? (list (car decode)))
 				(let ((operator (car execute)))
 					(printf "~s" (list? (cadr execute)))
-					(printf " ~s" ((symbol-get operator) (cadr execute) (caddr execute)) )
+					;(printf " ~s" ((symbol-get operator) (cadr execute) (caddr execute)) )
 					
 				)
 			)
