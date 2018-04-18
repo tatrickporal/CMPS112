26ef866e50d12d39076464c284b644c830b7c10f
pt <ptoral32@gmail.com>
2018-04-17 23:21:13 -0700

    sbi

--- asg1/sbi.scm
+++ asg1/sbi.scm
@@ -116,8 +116,8 @@
 						((list? (cadr math))
 							(cond 
 								((list? (caddr math))
-									(let ((arg1) (execute (list(cadr math))))
-										(let ((arg2) (execute (list(caddr math))))
+									(let ((arg1 (execute (list(cadr math)))))
+										(let ((arg2 (execute (list(caddr math)))))
 										(printf " ~s" ((symbol-get operator) arg1 arg2 ) )
 										)
 									
