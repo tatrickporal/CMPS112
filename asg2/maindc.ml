8538e49d8e31d1d510b79660db17f82ddcc1bfab
pt <ptoral32@gmail.com>
2018-04-26 13:49:08 -0700

    work

--- asg2/maindc.ml
+++ asg2/maindc.ml
@@ -63,7 +63,7 @@ let toploop (thestack: stack_t) inputchannel =
                  | Operator oper       -> execute thestack oper
                  );
              toploop ()
-        with End_of_file -> printf "End_of_file\n%!";
+        with End_of_file -> printf "";
     in  toploop ()
 
 let readfiles () =
