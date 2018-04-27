057d8f840b0c6f92b7ad6846b4ec2ac3e9e1dc5f
pt <ptoral32@gmail.com>
2018-04-27 16:27:47 -0700

    Work

--- asg2/bigint.mli
+++ asg2/bigint.mli
@@ -5,7 +5,7 @@ module Bigint : sig
    val bigint_of_string : string -> bigint
    val string_of_bigint : bigint -> string
    val add : bigint -> bigint -> bigint
-   val sub : bigint -> bigint -> bigint
+   val sub : string
    val mul : bigint -> bigint -> bigint
    val div : bigint -> bigint -> bigint
    val rem : bigint -> bigint -> bigint
