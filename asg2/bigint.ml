c4b1ab0e6aba80fc790e6eb6dfad20724624f681
pt <ptoral32@gmail.com>
2018-04-29 16:22:16 -0700

    Work

--- asg2/bigint.ml
+++ asg2/bigint.ml
@@ -66,7 +66,7 @@ module Bigint = struct
         | list1, [], carry   -> add' list1 [carry] 0
         | [], list2, carry   -> add' [carry] list2 0
         | car1::cdr1, car2::cdr2, carry ->
-        | prinf "This is sorta working\n" 
+          prinf "This is sorta working\n" 
           let sum = car1 + car2 + carry
           in  sum mod radix :: add' cdr1 cdr2 (sum / radix)
 
