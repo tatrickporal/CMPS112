be6b3387b2f8271d87d9b2aa05527c163932c5ad
pt <ptoral32@gmail.com>
2018-04-30 11:21:40 -0700

    Work

--- asg2/maindc.ml
+++ asg2/maindc.ml
@@ -18,7 +18,7 @@ let print_number number =
     let string_number = string_of_bigint number in 
     let string_length = strlen string_number in 
     if (string_length < 69)
-    then printf "%s\n!"
+    then printf "%s\n!" string_number
     else printf "0\n"
     
     
