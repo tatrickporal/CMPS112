08efebebea7cf888ef9dffc106243bb8ddb90b05
pt <ptoral32@gmail.com>
2018-04-30 19:32:05 -0700

    Work

--- asg2/maindc.ml
+++ asg2/maindc.ml
@@ -17,7 +17,7 @@ type binop_t = bigint -> bigint -> bigint
 let rec print_number' (string_length , string_number, index) = 
     let remaining_length = string_length - index in 
             if(remaining_length >= 69) then
-            (printf "%s\\\n" (String.sub index string_number 69);
+            (printf "%s\\\n" (String.sub string_number index 69);
                 (print_number' (index + 69)))
             else printf "%s\n!" (String.sub index remaining_length string_number)
 
