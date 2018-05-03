2785cbec2436b0d5639f47c89e1211f20676a316
pt <ptoral32@gmail.com>
2018-05-02 23:14:03 -0700

    work

--- asg2/bigint.ml
+++ asg2/bigint.ml
@@ -108,7 +108,7 @@ module Bigint = struct
 
     let mul (Bigint (neg1, value1)) (Bigint (neg2, value2)) =
     if neg1 = neg2 
-        then let remainder, answer = mul' value1 value2 [1] in Bigint(Pos, answer)
+        then printf "at least lol" in let remainder, answer = mul' value1 value2 [1] in Bigint(Pos, answer)
     else 
         let remainder,answer = mul' value1 value2 [1] in Bigint(Neg,answer)
 
