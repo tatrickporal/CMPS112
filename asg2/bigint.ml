9109c58c6be4dc6f7728f4391e82d3ebc285ce07
pt <ptoral32@gmail.com>
2018-05-02 22:51:20 -0700

    work

--- asg2/bigint.ml
+++ asg2/bigint.ml
@@ -101,16 +101,16 @@ module Bigint = struct
 
     let two_times num =  add' num num 0   
 
-    let rec mul' list1 list2 p2 = 
-    let remainder, product = mul' list1 (two_times list2) (two_times p2) in
+    let rec mul' list1 p2 list2 = 
+    let remainder, product = mul' list1 (two_times p2) (two_times list2)  in
         if((cmp' remainder p2) < 0 ) then remainder, product
         else (sub' remainder p2 0), (add' product list2 0)
 
     let mul (Bigint (neg1, value1)) (Bigint (neg2, value2)) =
     if neg1 = neg2 
-        then let product = mul' value1 value2 [1] in Bigint(neg1,product )
+        then let product = mul' value1 [1] value2 in Bigint(neg1,product )
     else 
-        let product = mul' value1 value2 [1] in Bigint(Neg,product )
+        let product = mul' value1 [1] value2 in Bigint(Neg,product )
 
 
     let div = add
