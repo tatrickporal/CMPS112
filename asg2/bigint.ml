dbdb9343d069f8e790455961521885cb12124fa0
pt <ptoral32@gmail.com>
2018-05-03 01:01:59 -0700

    work

--- asg2/bigint.ml
+++ asg2/bigint.ml
@@ -105,9 +105,7 @@ module Bigint = struct
     let two_times num =  add' num num 0   
 
     let rec mul' list1 list2 p2 = 
-    if concat_list p2 > concat_list list1
-then list1, [0]
-    else let remainder, product = mul' list1 (two_times list2) (two_times p2) in
+    let remainder, product = mul' list1 (two_times list2) (two_times p2) in
         if(concat_list remainder < concat_list p2) then remainder, product
         else (sub' remainder p2 0), (add' product list2 0)
 
