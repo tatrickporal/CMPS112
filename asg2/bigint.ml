ce6dbc115d3c0ca8a101e4e98a7bb03d542b0e18
pt <ptoral32@gmail.com>
2018-05-03 03:22:56 -0700

    work

--- asg2/bigint.ml
+++ asg2/bigint.ml
@@ -114,8 +114,8 @@ module Bigint = struct
     let two_times num =  add' num num 0   
 
     let rec mul' list1 p2 list2 = 
-    if (cmp' list1 p2) = -1
-        then list1,[]
+    if (cmp' list1 p2) = 1
+        then [], list1
     else let left,right = mul' list1 (two_times p2) (two_times list2) in
     if (cmp' right list2) = -1 then left,right
     else (add' left p2 0), (sub' right list2 0) 
