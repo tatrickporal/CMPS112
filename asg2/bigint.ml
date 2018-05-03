371432bc0274abc7b0e4f0ae14f4dd5986a43437
pt <ptoral32@gmail.com>
2018-05-02 23:10:29 -0700

    work

--- asg2/bigint.ml
+++ asg2/bigint.ml
@@ -108,7 +108,7 @@ module Bigint = struct
 
     let mul (Bigint (neg1, value1)) (Bigint (neg2, value2)) =
     if neg1 = neg2 
-        then  Bigint(Pos, mul' value1 value2 [1])
+        then let rem, answer = mul' value1 value2 [1] in Bigint(Pos, answer )
     else 
         let product = mul' value1 value2 1 in Bigint(Neg,product )
 
