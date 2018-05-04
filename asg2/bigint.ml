01c4fb4782e80e030a6c066ba58bdd2661e4ccd6
pt <ptoral32@gmail.com>
2018-05-03 20:13:54 -0700

    work

--- asg2/bigint.ml
+++ asg2/bigint.ml
@@ -116,11 +116,11 @@ module Bigint = struct
     let rec mul' list1 p2 list2 = 
     (*Checking if p2 is > list1 BASECASE*)
     if(cmp' p2 list1) = 1
-    then [1],list2
+    then list1,[0]
     (* Else go into recursion  *)
-    else let left,right = mul' list1 (two_times p2) (two_times list2) in
-    if (cmp' p2 right) = 1 then left,right
-    else (sub' left list1 0),(add' list2 right 0)
+    else let remainder,product = mul' list1 (two_times p2) (two_times list2) in
+    if (cmp' p2 remainder) = 1 then remainder,product
+    else (sub' remainder p2 0),(add' product list2 0)
     
   
     let mul (Bigint (neg1, value1)) (Bigint (neg2, value2)) =
