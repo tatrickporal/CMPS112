fc9ee5a33fbb76bc92a4477bdde736ce185bc910
pt <ptoral32@gmail.com>
2018-05-03 23:25:03 -0700

    work

--- asg2/bigint.ml
+++ asg2/bigint.ml
@@ -156,7 +156,8 @@ module Bigint = struct
     let rec pow' base expt result = match expt with 
     |0 -> result
     |expt -> let _,product = mul' base [1] base 
-    
+
+
              in let quotient,_ = div expt [2] 
              in pow' product quotient result
     
